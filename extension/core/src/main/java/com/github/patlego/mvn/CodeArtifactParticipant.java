0a1f9459a6f4a3bd6a3223ebfcb220c3fda2133f
pat-lego <patrique.legault@gmail.com>
2021-07-09 09:43:28 -0400

    Trying other things

--- extension/core/src/main/java/com/github/patlego/mvn/CodeArtifactParticipant.java
+++ extension/core/src/main/java/com/github/patlego/mvn/CodeArtifactParticipant.java
@@ -1,56 +1,79 @@
 package com.github.patlego.mvn;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 
-import org.apache.maven.AbstractMavenLifecycleParticipant;
-import org.apache.maven.MavenExecutionException;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.settings.Server;
+import javax.inject.Inject;
+import org.apache.maven.properties.internal.SystemProperties;
+import org.apache.maven.settings.DefaultMavenSettingsBuilder;
+import org.apache.maven.settings.MavenSettingsBuilder;
+import org.apache.maven.settings.Settings;
+import org.apache.maven.settings.building.DefaultSettingsBuildingRequest;
+import org.apache.maven.settings.building.SettingsBuilder;
+import org.apache.maven.settings.building.SettingsBuildingException;
+import org.apache.maven.settings.building.SettingsBuildingRequest;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
-
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.slf4j.Logger;
 
-@Component(role = AbstractMavenLifecycleParticipant.class, hint = "servers")
-public class CodeArtifactParticipant extends AbstractMavenLifecycleParticipant {
+@Component(role = MavenSettingsBuilder.class, hint = "newBuilder")
+public class CodeArtifactParticipant extends DefaultMavenSettingsBuilder {
 
     @Requirement
     private Logger logger;
 
-    @Override
-    public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
-        
-        // for (Server server : session.getSettings().getServers()) {
-        //     logger.info("Found the " + server.getId());
-        //     server.setPassword(
-        //             "eyJ2ZXIiOjEsImlzdSI6MTYyNTc3MTUxNSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6InJKdUJEaGphOVdJQkRqV21wQWxhcVEiLCJleHAiOjE2MjU4MTQ3MTUsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiUkZaMHIwbWNaWWY3OHlwMiJ9.z_YjYMoCBSwxr_l3lNqoUg.lZNrbc5yuh2FHBRt.sJ4D08WWoRHFQy2ZvvFkpPRR3LrqEdycykaAtKip2nOuSzDL7NTMi8VsQZdkPe5BsLhc86suK99cu4np8YP1iVL_Rgz6STlnqJH1DgttynAiIUFFQsePcfrybS7-Sbgyd1UkVKNw2i6PCWcrtHYO2wZT1y1BRP_8KWnJkgjovT5rv_GnKo2k9ptjXTS4gl6Y2pI1dLEXKavPi7mYNoXIbVKDkcY16Ys_JpqS-bVA6tN183lpNTNpfV8Vo0eAV6jF0YJ9-VgASR8_hLPxtFTR8dsjkLeKFZWZ8J3aGiNyWxYSA6uI77xQVGI_LFrqDWCDFHDdz923zkiL_S8oCbq-61DjDwTzp8bxLVuLAytXTfNnGoms0cAlYRYXiJ1IejmthlcrWanr0aKCr5un3eVZd98dA1GCNMqedGBsoGh6tfMcILCpbrf6ErPgHpJUrl_Ky9C9OwikxlDA2VQx2aLmVO_VF-HhAiTS_8K5RVqZQ-zOawAE_npR-HVNBpsHL33xfeyZDv96p4dOLjxK81XD7lluhUDxr5hhEba85kD4UZ-mb7OslkxVGpJTKJYr2jge-MrzuA0wKcc3NbSvDkKv110nhwQh-mUIPEvGYBUaA1L0TwPaGCQqcCrhLsOavc0idcn8ATnHWvhG7ts8AvG7u_e54W9pNutqbYAbOAUCpOYRY97hzqtMx3Y63xBhPIMCs8vPW7Rdl1PAOq8AlEU9krgMggLLV8k02eH7iy0HMIj7WC36NHT7VZnfONwz-g77ZVcT-vpS4Cxau6xSZ8caGsEsKcz33vAUBUP0xDszO7YQocvFH4MbRUHO8kPVfC_4B6QXLiSsMtSGq2XRbHn4KYqH8CKAWGGKjOYG3NFBG0oXZIvU3NS_vehsf5aDkTd3Crjk-4ZFiw6yQeAvp-ldS0WfDiUwhvnTjMJjrRlM6EWsNw8tJ56JjQ._uuBV6rnnz9wNzkMxcj7TQ");
-        // }
-        // logger.info(session.getSystemProperties().getProperty("ext.pwd"));
-        // session.getSystemProperties().setProperty("ext.pwd", "eyJ2ZXIiOjEsImlzdSI6MTYyNTc3MTUxNSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6InJKdUJEaGphOVdJQkRqV21wQWxhcVEiLCJleHAiOjE2MjU4MTQ3MTUsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiUkZaMHIwbWNaWWY3OHlwMiJ9.z_YjYMoCBSwxr_l3lNqoUg.lZNrbc5yuh2FHBRt.sJ4D08WWoRHFQy2ZvvFkpPRR3LrqEdycykaAtKip2nOuSzDL7NTMi8VsQZdkPe5BsLhc86suK99cu4np8YP1iVL_Rgz6STlnqJH1DgttynAiIUFFQsePcfrybS7-Sbgyd1UkVKNw2i6PCWcrtHYO2wZT1y1BRP_8KWnJkgjovT5rv_GnKo2k9ptjXTS4gl6Y2pI1dLEXKavPi7mYNoXIbVKDkcY16Ys_JpqS-bVA6tN183lpNTNpfV8Vo0eAV6jF0YJ9-VgASR8_hLPxtFTR8dsjkLeKFZWZ8J3aGiNyWxYSA6uI77xQVGI_LFrqDWCDFHDdz923zkiL_S8oCbq-61DjDwTzp8bxLVuLAytXTfNnGoms0cAlYRYXiJ1IejmthlcrWanr0aKCr5un3eVZd98dA1GCNMqedGBsoGh6tfMcILCpbrf6ErPgHpJUrl_Ky9C9OwikxlDA2VQx2aLmVO_VF-HhAiTS_8K5RVqZQ-zOawAE_npR-HVNBpsHL33xfeyZDv96p4dOLjxK81XD7lluhUDxr5hhEba85kD4UZ-mb7OslkxVGpJTKJYr2jge-MrzuA0wKcc3NbSvDkKv110nhwQh-mUIPEvGYBUaA1L0TwPaGCQqcCrhLsOavc0idcn8ATnHWvhG7ts8AvG7u_e54W9pNutqbYAbOAUCpOYRY97hzqtMx3Y63xBhPIMCs8vPW7Rdl1PAOq8AlEU9krgMggLLV8k02eH7iy0HMIj7WC36NHT7VZnfONwz-g77ZVcT-vpS4Cxau6xSZ8caGsEsKcz33vAUBUP0xDszO7YQocvFH4MbRUHO8kPVfC_4B6QXLiSsMtSGq2XRbHn4KYqH8CKAWGGKjOYG3NFBG0oXZIvU3NS_vehsf5aDkTd3Crjk-4ZFiw6yQeAvp-ldS0WfDiUwhvnTjMJjrRlM6EWsNw8tJ56JjQ._uuBV6rnnz9wNzkMxcj7TQ");
-        // logger.info(session.getSystemProperties().getProperty("ext.pwd"));
-        
-        // for (MavenProject project : session.getAllProjects()) {
-        //     project.getProperties().put("settings.servers.aftia-internal.password",
-        //             "eyJ2ZXIiOjEsImlzdSI6MTYyNTc3MTUxNSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6InJKdUJEaGphOVdJQkRqV21wQWxhcVEiLCJleHAiOjE2MjU4MTQ3MTUsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiUkZaMHIwbWNaWWY3OHlwMiJ9.z_YjYMoCBSwxr_l3lNqoUg.lZNrbc5yuh2FHBRt.sJ4D08WWoRHFQy2ZvvFkpPRR3LrqEdycykaAtKip2nOuSzDL7NTMi8VsQZdkPe5BsLhc86suK99cu4np8YP1iVL_Rgz6STlnqJH1DgttynAiIUFFQsePcfrybS7-Sbgyd1UkVKNw2i6PCWcrtHYO2wZT1y1BRP_8KWnJkgjovT5rv_GnKo2k9ptjXTS4gl6Y2pI1dLEXKavPi7mYNoXIbVKDkcY16Ys_JpqS-bVA6tN183lpNTNpfV8Vo0eAV6jF0YJ9-VgASR8_hLPxtFTR8dsjkLeKFZWZ8J3aGiNyWxYSA6uI77xQVGI_LFrqDWCDFHDdz923zkiL_S8oCbq-61DjDwTzp8bxLVuLAytXTfNnGoms0cAlYRYXiJ1IejmthlcrWanr0aKCr5un3eVZd98dA1GCNMqedGBsoGh6tfMcILCpbrf6ErPgHpJUrl_Ky9C9OwikxlDA2VQx2aLmVO_VF-HhAiTS_8K5RVqZQ-zOawAE_npR-HVNBpsHL33xfeyZDv96p4dOLjxK81XD7lluhUDxr5hhEba85kD4UZ-mb7OslkxVGpJTKJYr2jge-MrzuA0wKcc3NbSvDkKv110nhwQh-mUIPEvGYBUaA1L0TwPaGCQqcCrhLsOavc0idcn8ATnHWvhG7ts8AvG7u_e54W9pNutqbYAbOAUCpOYRY97hzqtMx3Y63xBhPIMCs8vPW7Rdl1PAOq8AlEU9krgMggLLV8k02eH7iy0HMIj7WC36NHT7VZnfONwz-g77ZVcT-vpS4Cxau6xSZ8caGsEsKcz33vAUBUP0xDszO7YQocvFH4MbRUHO8kPVfC_4B6QXLiSsMtSGq2XRbHn4KYqH8CKAWGGKjOYG3NFBG0oXZIvU3NS_vehsf5aDkTd3Crjk-4ZFiw6yQeAvp-ldS0WfDiUwhvnTjMJjrRlM6EWsNw8tJ56JjQ._uuBV6rnnz9wNzkMxcj7TQ");
-
-        //             project.getProperties().put("settings.servers.server.aftia-internal.password",
-        //             "eyJ2ZXIiOjEsImlzdSI6MTYyNTc3MTUxNSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6InJKdUJEaGphOVdJQkRqV21wQWxhcVEiLCJleHAiOjE2MjU4MTQ3MTUsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiUkZaMHIwbWNaWWY3OHlwMiJ9.z_YjYMoCBSwxr_l3lNqoUg.lZNrbc5yuh2FHBRt.sJ4D08WWoRHFQy2ZvvFkpPRR3LrqEdycykaAtKip2nOuSzDL7NTMi8VsQZdkPe5BsLhc86suK99cu4np8YP1iVL_Rgz6STlnqJH1DgttynAiIUFFQsePcfrybS7-Sbgyd1UkVKNw2i6PCWcrtHYO2wZT1y1BRP_8KWnJkgjovT5rv_GnKo2k9ptjXTS4gl6Y2pI1dLEXKavPi7mYNoXIbVKDkcY16Ys_JpqS-bVA6tN183lpNTNpfV8Vo0eAV6jF0YJ9-VgASR8_hLPxtFTR8dsjkLeKFZWZ8J3aGiNyWxYSA6uI77xQVGI_LFrqDWCDFHDdz923zkiL_S8oCbq-61DjDwTzp8bxLVuLAytXTfNnGoms0cAlYRYXiJ1IejmthlcrWanr0aKCr5un3eVZd98dA1GCNMqedGBsoGh6tfMcILCpbrf6ErPgHpJUrl_Ky9C9OwikxlDA2VQx2aLmVO_VF-HhAiTS_8K5RVqZQ-zOawAE_npR-HVNBpsHL33xfeyZDv96p4dOLjxK81XD7lluhUDxr5hhEba85kD4UZ-mb7OslkxVGpJTKJYr2jge-MrzuA0wKcc3NbSvDkKv110nhwQh-mUIPEvGYBUaA1L0TwPaGCQqcCrhLsOavc0idcn8ATnHWvhG7ts8AvG7u_e54W9pNutqbYAbOAUCpOYRY97hzqtMx3Y63xBhPIMCs8vPW7Rdl1PAOq8AlEU9krgMggLLV8k02eH7iy0HMIj7WC36NHT7VZnfONwz-g77ZVcT-vpS4Cxau6xSZ8caGsEsKcz33vAUBUP0xDszO7YQocvFH4MbRUHO8kPVfC_4B6QXLiSsMtSGq2XRbHn4KYqH8CKAWGGKjOYG3NFBG0oXZIvU3NS_vehsf5aDkTd3Crjk-4ZFiw6yQeAvp-ldS0WfDiUwhvnTjMJjrRlM6EWsNw8tJ56JjQ._uuBV6rnnz9wNzkMxcj7TQ");
-
-        // }
-    }
+    @Inject
+    private SettingsBuilder settingsBuilder;
 
     @Override
-    public void afterSessionStart(MavenSession session) throws MavenExecutionException {
-        logger.info(String.format("%s - About to set the repository tag password to the appropriate value",
-                Constants.EXT_NAME));
+    public Settings buildSettings(File userSettingsFile) throws IOException, XmlPullParserException {
+        logger.info("Running the new builder");
 
-        logger.info(session.getSystemProperties().getProperty("ext.pwd"));
-        session.getSystemProperties().setProperty("ext.pwd", "eyJ2ZXIiOjEsImlzdSI6MTYyNTc3MTUxNSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6InJKdUJEaGphOVdJQkRqV21wQWxhcVEiLCJleHAiOjE2MjU4MTQ3MTUsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiUkZaMHIwbWNaWWY3OHlwMiJ9.z_YjYMoCBSwxr_l3lNqoUg.lZNrbc5yuh2FHBRt.sJ4D08WWoRHFQy2ZvvFkpPRR3LrqEdycykaAtKip2nOuSzDL7NTMi8VsQZdkPe5BsLhc86suK99cu4np8YP1iVL_Rgz6STlnqJH1DgttynAiIUFFQsePcfrybS7-Sbgyd1UkVKNw2i6PCWcrtHYO2wZT1y1BRP_8KWnJkgjovT5rv_GnKo2k9ptjXTS4gl6Y2pI1dLEXKavPi7mYNoXIbVKDkcY16Ys_JpqS-bVA6tN183lpNTNpfV8Vo0eAV6jF0YJ9-VgASR8_hLPxtFTR8dsjkLeKFZWZ8J3aGiNyWxYSA6uI77xQVGI_LFrqDWCDFHDdz923zkiL_S8oCbq-61DjDwTzp8bxLVuLAytXTfNnGoms0cAlYRYXiJ1IejmthlcrWanr0aKCr5un3eVZd98dA1GCNMqedGBsoGh6tfMcILCpbrf6ErPgHpJUrl_Ky9C9OwikxlDA2VQx2aLmVO_VF-HhAiTS_8K5RVqZQ-zOawAE_npR-HVNBpsHL33xfeyZDv96p4dOLjxK81XD7lluhUDxr5hhEba85kD4UZ-mb7OslkxVGpJTKJYr2jge-MrzuA0wKcc3NbSvDkKv110nhwQh-mUIPEvGYBUaA1L0TwPaGCQqcCrhLsOavc0idcn8ATnHWvhG7ts8AvG7u_e54W9pNutqbYAbOAUCpOYRY97hzqtMx3Y63xBhPIMCs8vPW7Rdl1PAOq8AlEU9krgMggLLV8k02eH7iy0HMIj7WC36NHT7VZnfONwz-g77ZVcT-vpS4Cxau6xSZ8caGsEsKcz33vAUBUP0xDszO7YQocvFH4MbRUHO8kPVfC_4B6QXLiSsMtSGq2XRbHn4KYqH8CKAWGGKjOYG3NFBG0oXZIvU3NS_vehsf5aDkTd3Crjk-4ZFiw6yQeAvp-ldS0WfDiUwhvnTjMJjrRlM6EWsNw8tJ56JjQ._uuBV6rnnz9wNzkMxcj7TQ");
-        logger.info(session.getSystemProperties().getProperty("ext.pwd"));
+        File globalSettingsFile = new File("/Users/patriquelegault/Desktop/newSettings.xml");
 
-        session.getSettings().getServer("aftia-internal").setPassword("eyJ2ZXIiOjEsImlzdSI6MTYyNTc3MTUxNSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6InJKdUJEaGphOVdJQkRqV21wQWxhcVEiLCJleHAiOjE2MjU4MTQ3MTUsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiUkZaMHIwbWNaWWY3OHlwMiJ9.z_YjYMoCBSwxr_l3lNqoUg.lZNrbc5yuh2FHBRt.sJ4D08WWoRHFQy2ZvvFkpPRR3LrqEdycykaAtKip2nOuSzDL7NTMi8VsQZdkPe5BsLhc86suK99cu4np8YP1iVL_Rgz6STlnqJH1DgttynAiIUFFQsePcfrybS7-Sbgyd1UkVKNw2i6PCWcrtHYO2wZT1y1BRP_8KWnJkgjovT5rv_GnKo2k9ptjXTS4gl6Y2pI1dLEXKavPi7mYNoXIbVKDkcY16Ys_JpqS-bVA6tN183lpNTNpfV8Vo0eAV6jF0YJ9-VgASR8_hLPxtFTR8dsjkLeKFZWZ8J3aGiNyWxYSA6uI77xQVGI_LFrqDWCDFHDdz923zkiL_S8oCbq-61DjDwTzp8bxLVuLAytXTfNnGoms0cAlYRYXiJ1IejmthlcrWanr0aKCr5un3eVZd98dA1GCNMqedGBsoGh6tfMcILCpbrf6ErPgHpJUrl_Ky9C9OwikxlDA2VQx2aLmVO_VF-HhAiTS_8K5RVqZQ-zOawAE_npR-HVNBpsHL33xfeyZDv96p4dOLjxK81XD7lluhUDxr5hhEba85kD4UZ-mb7OslkxVGpJTKJYr2jge-MrzuA0wKcc3NbSvDkKv110nhwQh-mUIPEvGYBUaA1L0TwPaGCQqcCrhLsOavc0idcn8ATnHWvhG7ts8AvG7u_e54W9pNutqbYAbOAUCpOYRY97hzqtMx3Y63xBhPIMCs8vPW7Rdl1PAOq8AlEU9krgMggLLV8k02eH7iy0HMIj7WC36NHT7VZnfONwz-g77ZVcT-vpS4Cxau6xSZ8caGsEsKcz33vAUBUP0xDszO7YQocvFH4MbRUHO8kPVfC_4B6QXLiSsMtSGq2XRbHn4KYqH8CKAWGGKjOYG3NFBG0oXZIvU3NS_vehsf5aDkTd3Crjk-4ZFiw6yQeAvp-ldS0WfDiUwhvnTjMJjrRlM6EWsNw8tJ56JjQ._uuBV6rnnz9wNzkMxcj7TQ");
-        
+        SettingsBuildingRequest request = new DefaultSettingsBuildingRequest();
+        request.setUserSettingsFile(userSettingsFile);
+        request.setGlobalSettingsFile(globalSettingsFile);
+        request.setSystemProperties(SystemProperties.getSystemProperties());
+        return build(request);
     }
+
+    private Settings build(SettingsBuildingRequest request) throws IOException, XmlPullParserException {
+        try {
+            return settingsBuilder.build(request).getEffectiveSettings();
+        } catch (SettingsBuildingException e) {
+            throw (IOException) new IOException(e.getMessage()).initCause(e);
+        }
+    }
+
+    // @Override
+    // public void afterProjectsRead(MavenSession session) throws
+    // MavenExecutionException {
+    // try {
+    // String settings = new String(
+    // "<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\"
+    // xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+    // xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0
+    // http://maven.apache.org/xsd/settings-1.0.0.xsd\"> <servers> <server>
+    // <id>github</id> <username>pat-lego</username>
+    // <password>44b2a9141ab5894bcac7e8a30ae1177e661281dd</password> </server>
+    // <server> <id>aemfaacs-aftia--aemfaacs</id> <username>aws</username>
+    // <password>${env.CODEARTIFACT_AUTH_TOKEN}</password> </server> <server>
+    // <id>aftia-first-american</id> <username>aws</username>
+    // <password>${env.CODEARTIFACT_AUTH_TOKEN}</password> </server> <server>
+    // <id>aftia-optum</id> <username>aws</username>
+    // <password>${env.CODEARTIFACT_AUTH_TOKEN}</password> </server> <server>
+    // <id>aftia-internal</id> <username>aws</username>
+    // <password>eyJ2ZXIiOjEsImlzdSI6MTYyNTgzNjU4MSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6Imxsc0VYcHdoZGdCRGxuVzlkWlZnemciLCJleHAiOjE2MjU4Nzk3ODEsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiM3RzM2VCNEhuNC1qMlRGeiJ9.MlKz9KdTwZrnEXW1ugSA1w.SXIK8n-ELcEvEtfZ.8eAO_X7eMdS-MX5SmAGaXtvR9f9KWis_dO83w_2Mh1ZHsscgootTNQIQRTm6pgLyETEjWBDHhRmnU6SDzB_Dgt85i5S2B5TgBbQGJZOmNYKw9lf6pcXBr7ZwgWJ6ivmIwfmVxwE6oaD-Wb_t9XXqWByaLi70gafGt_Yt_NWt7iTPs8VaD7E12DZaYc9avjh6K1OLX5a49QVINiOmkHlnsisRHfv-gsyp1ig_Hi4HBtr_vVLcn8Jv8grQMW-8yqbbZ6BAkTwJcEXS7wGV7lrjiI_nvJn9zqjaRB3-0iOkTntE2jerGG633mpGdE9hK3s2XJxnvPr-FCTjNDjXwVIzukio2rvzEaIjHOePO6XgWrADCgOMd_YEB2cj0f12ieE6XQWQvgPhG2-3XT5Int4AWWB5YOtC-ckfgUd-ADCETZCfMf_VytNMvqtmMpi2I3xJ-TafeUECEDQof8ZGumRDfBLwud6uvCMTgm4_DiM-6XhAVRqM6eLYq7NDKavDvtBcPcOafpAmCLo4v7U36RbyBlO0i2xDRMSuOiBunPc7NyWa3GYwttT47X4tplJJarAjSX8OuyhfSPRKwzptFh99EOBlbhZT9ECGfhe5wZvh5mx4XlqsOee58X5BBr4ztXOPB5VfkD_6_OA0noGH_6p8xs2L3xZLTbo_zz3SVNL_B0yeJxjg8Ns8Mc1ZRV3CIRnxPmhp0FUjNUFhNU5dDVegzpyFbt_bg9V04i4gWppwuoTkJUiWNJ6QaYiMbaOlJ1R7srVVHplX15tauPXa87AemSjwkSqXPD97KovcAnDsPVpewBpZ1dHS1f-caWt-Z7mdi4fyva9ZcngOgS469ujwjffCoxqczQjpUzV3V07ydor6z4bFXfVuccbCfoLC5J2FZF9Gqe2SqxjNrEQh7xb_n3TtGCl0mS8Kv8fcesBstfRKRWcEYuRK.GaysUo3sH1iti81GQbNwYQ</password>
+    // </server> </servers> </settings>");
+
+    // Files.write(Paths.get("/Users/patriquelegault/.m2/settings.xml"),
+    // settings.getBytes());
+    // } catch (IOException e) {
+    // logger.error(e.getMessage(), e);
+    // throw new MavenExecutionException(e.getMessage(), e);
+    // }
+
+    // }
 }
